baf46122fb4bda63f55e87f5d0e486d064319bb4
SungHo Lee <shlee@unc.edu>
2020-04-30 16:23:33 -0400

    minor update

--- pynipt/__init__.py
+++ pynipt/__init__.py
@@ -1,18 +1,11 @@
 # Python NeuroImage Pipeline Tool
-import os
-# import sys
-import shutil
 from .lib import plugin
 from .lib.bucket import Bucket
 from .lib.interface import InterfaceBuilder
 from .lib.pipeline import PipelineBuilder, Pipeline
 from .lib.processor import Processor
 
-from .config import config as __config
-from .config import cfg_path as __cfg_path, \
-    create_config_file as __make_config, \
-    restore_config
-from .utils import intensive_mkdir as __mkdir
+from .config import config
 from paralexe import Scheduler
 
 __version__ = '0.1.1b0'
